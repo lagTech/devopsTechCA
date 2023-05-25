c65aab18073a390bd4f9fe0de49fc9a14603d7a1
lagTech <108477716+lagTech@users.noreply.github.com>
2023-05-25 16:36:09 +0100

    Add files via upload

---
+++ my_script.py
@@ -0,0 +1,7 @@
+class MyClass:
+    def greet(self, name):
+        return f"Hello, {name}!"
+
+
+def add_num(A, B):
+    return A + B
---
+++ the_newscript.py
@@ -0,0 +1,14 @@
+import fire
+
+
+class MyClass:
+    def greet(self, name):
+        return f"Hello, {name}!"
+
+
+def add_num(A, B):
+    return A + B
+
+
+if __name__ == '__main__':
+    fire.Fire()
